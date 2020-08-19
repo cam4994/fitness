ac6c854a4152fd268c6b2cfd8e20599c4b109cf7
cam4994 <colin.a.mosley@gmail.com>
2020-08-19 12:15:14 -0500

    Added number of sets to plans

--- app/models/exercise.rb
+++ app/models/exercise.rb
@@ -1,4 +1,6 @@
 class Exercise < ApplicationRecord
     has_many :plan_exercises
     has_many :plans, through: :plan_exercises
+
+
 end
--- app/models/plan_exercise.rb
+++ app/models/plan_exercise.rb
@@ -1,4 +1,8 @@
 class PlanExercise < ApplicationRecord
     belongs_to :plan 
     belongs_to :exercise
+
+    def exercise_name
+        self.exercise.name
+    end
 end
