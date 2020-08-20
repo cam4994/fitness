0929fc48ed4fecb3248e8c83fb2768af859aba48
cam4994 <colin.a.mosley@gmail.com>
2020-08-20 11:06:29 -0500

    fixed minor bugs

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   resources :exercises, :plans, :user_plans, :users
   get '/', to: 'sessions#new'
   post '/login', to: 'sessions#create'
-  delete '/signout', to: 'session#delete'
+  delete '/signout', to: 'sessions#destroy'
 end
